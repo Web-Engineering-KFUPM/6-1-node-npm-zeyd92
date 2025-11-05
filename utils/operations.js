88e8179f251a25dfb65e7393eaa38e60238590c9
zeyd92 <zeyd123a@gmail.com>
2025-11-05 20:50:43 +0300

    mn

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,16 @@
 export function add(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((sum, n) => sum + n, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+  return numbers.slice(1).reduce((acc, n) => acc - n, numbers[0]);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((acc, n) => acc * n, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
+  return numbers.slice(1).reduce((acc, n) => acc / n, numbers[0]);
 }
 
